40fec7f4d328f82b919076622c3d90b4826d0050
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2025-05-21 15:06:04 +0530

    feat: add logs for mark-compact functions

--- src/Mark Compact/test.c
+++ src/Mark Compact/test.c
@@ -2,43 +2,43 @@
 #include <stdlib.h>
 #include "gc.h"
 
-typedef struct Node{
-  char data;
-  struct Node *left, *right;
-} Node;
+  typedef struct Node{
+    char data;
+    struct Node *left, *right;
+  } Node;
 
-Node *create_graph(){
-  Node *a = (Node *)gc_malloc(sizeof(Node));
-  Node *b = (Node *)gc_malloc(sizeof(Node));
-  Node *c = (Node *)gc_malloc(sizeof(Node));
-  Node *d = (Node *)gc_malloc(sizeof(Node));
-  Node *e = (Node *)gc_malloc(sizeof(Node));
-  Node *f = (Node *)gc_malloc(sizeof(Node));
-  Node *g = (Node *)gc_malloc(sizeof(Node));
-  Node *h = (Node *)gc_malloc(sizeof(Node));
+  Node *create_graph(){
+    Node *a = (Node *)gc_malloc(sizeof(Node));
+    Node *b = (Node *)gc_malloc(sizeof(Node));
+    Node *c = (Node *)gc_malloc(sizeof(Node));
+    Node *d = (Node *)gc_malloc(sizeof(Node));
+    Node *e = (Node *)gc_malloc(sizeof(Node));
+    Node *f = (Node *)gc_malloc(sizeof(Node));
+    Node *g = (Node *)gc_malloc(sizeof(Node));
+    Node *h = (Node *)gc_malloc(sizeof(Node));
 
-  a->data = 'a';
-  b->data = 'b';
-  c->data = 'c';
-  d->data = 'd';
-  e->data = 'e';
-  f->data = 'f';
-  g->data = 'g';
-  h->data = 'h';
+    a->data = 'a';
+    b->data = 'b';
+    c->data = 'c';
+    d->data = 'd';
+    e->data = 'e';
+    f->data = 'f';
+    g->data = 'g';
+    h->data = 'h';
 
-  a->left = b;
-  a->right = c;
+    a->left = b;
+    a->right = c;
 
-  c->right = e;
-  c->left = d;
+    c->right = e;
+    c->left = d;
 
-  e->left = f;
-  e->right = g;
+    e->left = f;
+    e->right = g;
 
-  g->right = h;
+    g->right = h;
 
-  return a;
-}
+    return a;
+  }
 
 void print_graph(Node *root){
   if(!root) return;
