b3675baaa79e788aceb5fb9ecc57ee9eb1d02ce7
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2025-05-24 19:05:16 +0530

    feat: started writing tests for mark compact

--- tests/Mark-Compact/test.c
+++ tests/Mark-Compact/test.c
@@ -43,8 +43,56 @@ void test_gc_malloc() {
     *ptr = 42;
     printf("Value at allocated memory: %d\n", *ptr);
 
+    if(gc.total_allocated == 0) {
+        printf("gc_malloc failed: total_allocated is not updated\n");
+    } else {
+        printf("gc_malloc successful: total_allocated is updated\n");
+    }
+
+    if(gc.list_head == NULL && gc.list_tail == NULL) {
+        printf("gc_malloc failed: list_head and list_tail are not updated\n");
+    } else {
+        printf("gc_malloc successful: list_head and list_tail are updated\n");
+    }
+
+    if(hashset_lookup(gc.address, (uintptr_t *)ptr) == 0) {
+        printf("gc_malloc failed: address not found in hashset\n");
+    } else {
+        printf("gc_malloc successful: address found in hashset\n");
+    }
+
+    if(hashmap_lookup(gc.metadata, (uintptr_t *)ptr) == 0) {
+        printf("gc_malloc failed: metadata not found in hashmap\n");
+    } else {
+        printf("gc_malloc successful: metadata found in hashmap\n");
+    }
+
+
+    gc_free(ptr);
+}
+
+void test_gc_free() {
+    int *ptr = (int *)gc_malloc(sizeof(int));
+    if(ptr == NULL) {
+        printf("gc_malloc failed: returned NULL\n");
+        exit(1);
+    }
+
     gc_free((uintptr_t *)ptr);
-    printf("Memory freed successfully\n");
+
+    if(hashset_lookup(gc.address, (uintptr_t *)ptr) != 0) {
+        printf("gc_free failed: address still found in hashset\n");
+    } else {
+        printf("gc_free successful: address not found in hashset\n");
+    }
+
+    if(hashmap_lookup(gc.metadata, (uintptr_t *)ptr) != 0) {
+        printf("gc_free failed: metadata still found in hashmap\n");
+    } else {
+        printf("gc_free successful: metadata not found in hashmap\n");
+    }
+
+    
 }
 
 int main(){
